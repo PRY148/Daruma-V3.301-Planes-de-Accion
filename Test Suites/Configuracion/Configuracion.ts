4388915d73c07f6e43a7175b55f722f7bd60b33c
Jessica <jecastro@darumasoftware.com>
2019-05-22 09:32:48 -0500

    Test Siut

---
+++ Test Suites/Configuracion/Configuracion.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Configuracion</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>41c3a9c7-4ed9-452f-bdcb-c185c3567553</testSuiteGuid>
+</TestSuiteEntity>
